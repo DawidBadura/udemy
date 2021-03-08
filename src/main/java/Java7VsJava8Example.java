fb4485a632b3aff5571458b99892c3930e686954
Dawid <dawid.badurakrk@gmail.com>
2021-03-08 22:20:42 +0100

    Consumer person example

--- src/main/java/Java7VsJava8Example.java
+++ src/main/java/Java7VsJava8Example.java
@@ -2,48 +2,72 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
+
+
 public class Java7VsJava8Example {
+    static Consumer<Person> c1 = (p) -> System.out.println(p);
+
+    static Consumer<Person> c2 = (p) -> System.out.println(p.getName().toUpperCase());
+
+    static Consumer<Person> c3 = (p) -> System.out.println(p.getHobbies());
+    static List<Person> personList = PersonRepository.getAllPersons();
+
+    static void printNameandHobbies() {
+        personList.forEach(c1.andThen(c2));
+    }
+
+    static void printWithCondition() {
+        personList.forEach(p -> { // iterate student
+            if(p.getGender().equals("Male") && p.getHeight() >=140) {
+                c2.andThen(c3).accept(p);
+            }
+        });
+    }
+
 
     public static void main(String[] args) {
         //Runable lambda example
         {
             // Before
-        Runnable t1 = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Thread 1");
-            }
-        };
+            Runnable t1 = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Thread 1");
+                }
+            };
 
-        new Thread(t1).start();
+            new Thread(t1).start();
 
-        // After
-        Runnable t2 = () -> {System.out.println("Thread 2");};
-        new Thread(t2).start();
+            // After
+            Runnable t2 = () -> {
+                System.out.println("Thread 2");
+            };
+            new Thread(t2).start();
 
-        Runnable t3 = () -> {System.out.println("Thread 3");
-            System.out.println("Thread 3.1");
-        };
+            Runnable t3 = () -> {
+                System.out.println("Thread 3");
+                System.out.println("Thread 3.1");
+            };
 
-        new Thread(t3).start();
+            new Thread(t3).start();
 
-        new Thread(new Runnable() {
+            new Thread(new Runnable() {
 
-            @Override
-            public void run() {
-                System.out.println("Thread 4");
+                @Override
+                public void run() {
+                    System.out.println("Thread 4");
 
-            }
-        }).start();
+                }
+            }).start();
 
-        new Thread(() -> {
-            System.out.println("Thread 5");
-            System.out.println("Thread 5.1");
-        }).start();
-    }}
-
+            new Thread(() -> {
+                System.out.println("Thread 5");
+                System.out.println("Thread 5.1");
+            }).start();
+        }
         ////Comparator lambda example
         {
             // Before
@@ -55,34 +79,45 @@ public class Java7VsJava8Example {
                 }
             };
 
-            System.out.println("Comparator1 :"+c1.compare(15, 15)); //0
+            System.out.println("Comparator1 :" + c1.compare(15, 15)); //0
 
             //After
             Comparator<Integer> c2 = (Integer x, Integer y) -> x.compareTo(y);
-            System.out.println("Comparator2 :"+c2.compare(25, 15)); // 1
+            System.out.println("Comparator2 :" + c2.compare(25, 15)); // 1
 
             Comparator<Integer> c3 = (x, y) -> x.compareTo(y);
-            System.out.println("Comparator3 :"+c3.compare(15, 25)); // -1
+            System.out.println("Comparator3 :" + c3.compare(15, 25)); // -1
         }
         {
-        List<String> names = Arrays.asList("Sam","Peter","Billings","Sam");
+            List<String> names = Arrays.asList("Sam", "Peter", "Billings", "Sam");
 
-        //Before
-        List<String> uniqueList = new ArrayList<>();
-        for(String name : names) {
-            if(!uniqueList.contains(name)) {
-                uniqueList.add(name);
+            //Before
+            List<String> uniqueList = new ArrayList<>();
+            for (String name : names) {
+                if (!uniqueList.contains(name)) {
+                    uniqueList.add(name);
+                }
             }
+            System.out.println("Unique List 1 :" + uniqueList);
+
+            //After
+            List<String> uniqueValues1 = names.stream()
+                    .distinct()
+                    .collect(Collectors.toList());
+            System.out.println("Unique List 2 :" + uniqueValues1);
         }
-        System.out.println("Unique List 1 :"+uniqueList);
-
-        //After
-        List<String> uniqueValues1 = names.stream()
-                .distinct()
-                .collect(Collectors.toList());
-        System.out.println("Unique List 2 :"+uniqueValues1);}
+        {
 
 
+            /*List<Person> personList = PersonRepository.getAllPersons();
+            c1.accept(PersonRepository.getPerson());
+            c2.accept(PersonRepository.getPerson());
+            c3.accept(PersonRepository.getPerson());
+            c1.andThen(c2).andThen(c3).accept(PersonRepository.getPerson());
+            printNameandHobbies();*/
+            printWithCondition();
 
+        }
+    }
 
 }
