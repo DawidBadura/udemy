eb1e951b124d437fb5e4129931d4984f73a66b01
Dawid <dawid.badurakrk@gmail.com>
2021-03-10 19:30:05 +0100

    predicate example

--- src/main/java/Java7VsJava8Example.java
+++ src/main/java/Java7VsJava8Example.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 
@@ -164,6 +165,29 @@ public class Java7VsJava8Example {
         //biconsumer example 2
         {
             printPersonDetails();
+        }
+        //predicate example
+        {
+            Predicate<Integer> lessThan = a -> (a <= 50);
+            Predicate<Integer> greaterThan = a -> (a >= 100);
+            Predicate<Integer> equalTo = a -> (a == 0);
+
+
+		/*boolean result = lessThan.test(15);
+		System.out.println("Result :"+result);*/
+
+            System.out.println("Greater Than :" + greaterThan.test(150));
+            System.out.println("Less Than :" + lessThan.test(150));
+            System.out.println("Equal :" + equalTo.test(0));
+
+            boolean result1 = lessThan.and(greaterThan).and(equalTo).test(150);
+            System.out.println("Result 1:" + result1);
+
+            boolean result2 = lessThan.or(greaterThan).test(150);
+            System.out.println("Result 2:" + result2);
+
+            boolean result3 = lessThan.or(greaterThan).negate().test(150);
+            System.out.println("Result 3:" + result3);
         }
     }
 
