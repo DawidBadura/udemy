c9c05205c08a9b4a9fbb1d0a95cda95cb4785bd4
Dawid <dawid.badurakrk@gmail.com>
2021-03-19 20:53:29 +0100

    SSexample 1

---
+++ src/main/java/SimpleStreamExample.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class SimpleStreamExample {
+    public static void main(String[] args) {
+        /*Stream<Integer> s1 = Stream.of(1,2,3,4,5,6);
+		s1.forEach(p-> System.out.println(p));
+		System.out.println("=================");
+		Stream<Integer> s2 = Stream.of(new Integer[] {1,2,3,4,5,6,});
+		s2.forEach(p-> System.out.println(p));*/
+
+        Predicate<Person> p1 = (per) -> per.getHeight() >=140 ;
+        Predicate<Person> p2 = (per) -> per.getGender().equals("Male");
+
+        Map<String, List<String>> personMap = PersonRepository
+                .getAllPersons() // iterate all persons
+                .stream() // Stream of Persons
+                .filter(p1) // Stream of Persons
+                .filter(p2) // Stream of Persons
+                .collect(Collectors
+                        .toMap(Person :: getName, Person :: getHobbies));
+        System.out.println("Person Map :"+personMap);
+    }
+}
