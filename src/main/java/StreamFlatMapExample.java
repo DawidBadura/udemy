e89e9a39ef45523e148254d0ec41b5de3f66a2c2
Dawid <dawid.badurakrk@gmail.com>
2021-03-21 18:18:29 +0100

    flat example 2

--- src/main/java/StreamFlatMapExample.java
+++ src/main/java/StreamFlatMapExample.java
@@ -3,6 +3,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamFlatMapExample {
+    static List<String> hobbies(List<Person> personsList){
+        return personsList.stream()
+                .map(Person :: getHobbies)
+                .flatMap(List :: stream)
+                .collect(Collectors.toList());
+    }
     public static void main(String[] args) {
         List<Integer> oddNumber = Arrays.asList(1,3,5,7);
         List<Integer> evenNumber = Arrays.asList(2,4,6,8);
@@ -13,5 +19,6 @@ public class StreamFlatMapExample {
                 .flatMap(list -> list.stream())
                 .collect(Collectors.toList());
         System.out.println("After Flatten :"+flattenList);
+        System.out.println("Hobbies Result :"+hobbies(PersonRepository.getAllPersons()));
     }
 }
